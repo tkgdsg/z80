d7650d93739458cf98ea53c70361e4d9f3ee5b06
nanitsuku <nanitsuku@outlook.jp>
2021-10-09 20:21:09 +0900

    specify filename

--- src/main/scala/z80/Top.scala
+++ src/main/scala/z80/Top.scala
@@ -3,7 +3,7 @@ package z80
 import chisel3._
 import chisel3.util._
 
-class Top extends Module {
+class Top(filename:String) extends Module {
   val io = IO(new Bundle {
     val exit = Output(Bool())
 //    val registers = new Registers 
@@ -11,7 +11,7 @@ class Top extends Module {
   })
   
   val core   = Module(new Core())
-  val memory = Module(new Memory())
+  val memory = Module(new Memory(filename))
 //  val decoder = Module(new Decoder())
   core.io.bus <> memory.io.imem
 //  core.io.dd <> decoder.io.dd
