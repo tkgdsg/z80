140c4806b2eea756d6550a2af39b7220cf75d3d8
nanitsuku <nanitsuku@outlook.jp>
2021-10-17 22:03:00 +0900

    fix unit test

--- src/main/scala/z80/z80Test.scala
+++ src/main/scala/z80/z80Test.scala
@@ -91,9 +91,9 @@ class hagehoge(c: ALU) extends PeekPokeTester(c) {
 }
 
 }
-
+/*
 class TopTestPeekPokeTester(c: Top) extends PeekPokeTester(c) {
-    while (peek(c.io.exit)==0) {
+    while (peek(c.io.halt)==0) {
 //        System.out.printf("%d\n", peekAt(c.core.regfiles_front, 7))
 //        System.out.println(peekAt(c.core.regfiles_front, 7))
 
@@ -105,7 +105,7 @@ class TopTestPeekPokeTester(c: Top) extends PeekPokeTester(c) {
         step(1)
     }
 }
-
+*/
 object TopGenerate extends App {
 //    chisel3.Driver.execute(args, () => new Top)
     (new chisel3.stage.ChiselStage).emitVerilog(
@@ -286,17 +286,17 @@ object TopTest extends App {
 //          for(i <- 1 to 10) {
 //        var regs = c.io.registers
         var m1 = 0
-        while(peek(c.io.exit)==0) { 
+        while(peek(c.core.io.bus.HALT_) == 1) { 
 //          peekAt(c.core.regfiles_front, 7)
 //            poke(,99)
 //            c.io.exit.peek
 //            peek("c_core_A")
 //peek(c.core.A)
 //peek(c.core.io.bus.addr)
-          if (m1==0 && peek(c.io.M1)==1) {
+          if (m1==0 && peek(c.core.io.bus.M1_)==1) {
 //          println(s"aaaa:${peek(c.io.registers.PC)}")
           }
-          m1 = peek(c.io.M1).toInt
+          m1 = peek(c.core.io.bus.M1_).toInt
 //          regs = c.io.registers
 //println(s"m1:${peek(c.io.M1)}")
 //peek("Top.core_io_bus_data")
@@ -337,7 +337,7 @@ class TopSupervisor(filename:String) extends Module {
 //  val A = Output(UInt(8.W))
   val regs_front = Output(Vec(8,UInt(8.W)))
   val regs_back = Output(Vec(8,UInt(8.W)))
-  val exit = Output(Bool())
+  val halt_ = Output(Bool())
   val machine_state = Output(UInt(8.W))
   val t_cycle = Output(UInt(8.W))
   val PC = Output(UInt(16.W))
@@ -364,7 +364,7 @@ class TopSupervisor(filename:String) extends Module {
 //  io.reg(i) := reg(i)
  }
 
- io.exit := WireDefault(0.B)
+ io.halt_ := WireDefault(0.B)
  io.machine_state := WireDefault(0.U(8.W))
  io.t_cycle := WireDefault(0.U(8.W))
  
@@ -377,7 +377,7 @@ class TopSupervisor(filename:String) extends Module {
  io.I := WireDefault(0.U(8.W))
 
 // BoringUtils.bore(top.core.A, Seq(io.A))
- BoringUtils.bore(top.io.exit, Seq(io.exit))
+ BoringUtils.bore(top.core.io.bus.HALT_ , Seq(io.halt_))
  BoringUtils.bore(top.core.machine_state, Seq(io.machine_state))
  BoringUtils.bore(top.core.m1_t_cycle, Seq(io.t_cycle))
  
@@ -427,13 +427,13 @@ object TopTest22 extends App {
           System.out.println(" PC  A  B  C  D  E  F  H  L  A' B' C' D' E' F' H' L'  SP   IX   IY  R  I IFF  IFF2 IM\n")
               val regs = List(c.top.core.A_op, c.top.core.B_op, c.top.core.C_op, c.top.core.D_op, c.top.core.E_op, c.top.core.F_op, c.top.core.H_op, c.top.core.L_op)
 //              pc = peek(c.io.PC).U
-              val sp = peek(c.io.SP).U
-              val ix = peek(c.io.IX).U
-              val iy = peek(c.io.IY).U
-              val r = peek(c.io.R).U
-              val i = peek(c.io.I).U
-              val iff = 0.U //peek(c.io.IFF).U
-              val iff2 = 0.U //peek(c.io.IFF2).U
+              var sp = peek(c.io.SP).U
+              var ix = peek(c.io.IX).U
+              var iy = peek(c.io.IY).U
+              var r = peek(c.io.R).U
+              var i = peek(c.io.I).U
+              var iff = 0.U //peek(c.io.IFF).U
+              var iff2 = 0.U //peek(c.io.IFF2).U
               /*
               val itt = if(first) {
                 first = false
@@ -443,7 +443,7 @@ object TopTest22 extends App {
                val itt = unit_test.initialize(regs.map( n => peek(c.io.regs_front(n.toInt)).U), regs.map( n => peek(c.io.regs_back(n.toInt)).U), pc.toInt, sp.toInt, ix.toInt, iy.toInt, iff.toInt, iff2.toInt,  r.toInt, i.toInt)
 //                unit_test.setInit(regs.map( n => peek(c.io.regs_front(n.toInt)).U), regs.map( n => peek(c.io.regs_back(n.toInt)).U), pc, ix, iy, 0.U, 0.U, r, i)
               var pcc = pc.toInt
-          while(peek(c.io.exit)==0) {
+          while(peek(c.io.halt_)==1) {
             val machine_state:Int = peek(c.io.machine_state).toInt
             val t_cycle:Int = peek(c.io.t_cycle).toInt
 
@@ -453,6 +453,13 @@ object TopTest22 extends App {
               System.out.println(s"${c.top.core.A_op}\n")
 */
               pc = peek(c.io.PC).U
+              sp = peek(c.io.SP).U
+              ix = peek(c.io.IX).U
+              iy = peek(c.io.IY).U
+              r = peek(c.io.R).U
+              i = peek(c.io.I).U
+              iff = 0.U //peek(c.io.IFF).U
+              iff2 = 0.U //peek(c.io.IFF2).U
               System.out.print("D ")
               System.out.print(f"$prev_pc%04X ")
 //:              System.out.print(s"${Integer.toString(peek(c.io.PC).toInt, 16)}%X ")
@@ -475,7 +482,7 @@ object TopTest22 extends App {
               System.out.print("U ")
               s.print()
 
-              if (! s.check(regs.map { n => peek(c.io.regs_front(n.toInt)).asUInt()}, regs.map { n => peek(c.io.regs_back(n.toInt)).asUInt()}, pc.toInt, ix.toInt, iy.toInt, iff.toInt, iff2.toInt, r.toInt, i.toInt)) {
+              if (! s.check(regs.map { n => peek(c.io.regs_front(n.toInt)).asUInt()}, regs.map { n => peek(c.io.regs_back(n.toInt)).asUInt()}, pc.toInt, sp.toInt, ix.toInt, iy.toInt, iff.toInt, iff2.toInt, r.toInt, i.toInt)) {
                 println("error")
               }
               cc = cc + 1
@@ -539,7 +546,7 @@ case class Status(pc:Integer, regs:Array[UInt], sp:Integer, ix:Integer, iy:Integ
    println
   }
 
-  def check(regs_f:List[UInt],regs_b:List[UInt], pc:Integer, ix:Integer, iy:Integer, iff:Integer, iff2:Integer, r:Integer, i:Integer): Boolean = {
+  def check(regs_f:List[UInt],regs_b:List[UInt], pc:Integer, sp:Integer, ix:Integer, iy:Integer, iff:Integer, iff2:Integer, r:Integer, i:Integer): Boolean = {
     for ( (s, d) <- regfiles zip Array.concat(regs_f.toArray, regs_b.toArray)) {
       if ( s.litValue != d.litValue ) {
       //  println(s,d)
@@ -548,6 +555,7 @@ case class Status(pc:Integer, regs:Array[UInt], sp:Integer, ix:Integer, iy:Integ
       }
 //      println(s,d)
     }
+    if ( sp != SP ) return false
     true 
   }
 }
@@ -566,6 +574,7 @@ class UnitTest(filename:String) {
     for (line <- bufferedSource.getLines()) yield {
       val comm = line.split("; ")
       val cols = line.split(" ")
+      if (line.startsWith(";")) expect_status.invalid = true
       if(cols(0).matches("[0-9a-fA-F]{4}")) {
         expect_status.PC = Integer.parseInt(cols(0), 16)
         expect_status.invalid = true
@@ -583,6 +592,32 @@ class UnitTest(filename:String) {
                   } else {
                     Integer.parseInt(ee, 16).asUInt()
                   }
+              } else {
+                if (index == 17) {
+                  expect_status.SP =
+                    if (ee == "NC") {
+                       prev_status.SP
+                    } else {
+                      Integer.parseInt(ee, 16)
+                    }
+                }
+                  /*
+                index match {
+                  case 17 =>  expect_status.SP = Integer.parseInt(ee, 16)
+                  case 18 =>
+                  case 19 =>
+                  case 20 =>
+                  case 21 =>
+                  case 22 =>
+                  case 23 =>
+                  case 24 =>
+                  case 25 =>
+                  case 26 =>
+                  case 27 =>
+                  case 28=>
+                  case _ => ""
+                } 
+                    */
               }
             }
 //            println
@@ -626,7 +661,7 @@ class UnitTest(filename:String) {
     initialize2
   }
 
-  def check(regs_f:List[UInt],regs_b:List[UInt], pc:Integer, ix:Integer, iy:Integer, iff:Integer, iff2:Integer, r:Integer, i:Integer): Boolean = {
+  def check(regs_f:List[UInt],regs_b:List[UInt], pc:Integer, sp:Integer, ix:Integer, iy:Integer, iff:Integer, iff2:Integer, r:Integer, i:Integer): Boolean = {
     for ( (s, d) <- prev_status.regfiles zip Array.concat(regs_f.toArray, regs_b.toArray)) {
       if ( s != d ) Breaks.break()
       false
